f23a2eb4d77fa106b27422cc477afa1871ba01b8
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 15:35:50 +0000

    Corrected intentional incorrect Jenkins address.

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,3 +1,3 @@
 node ('master') {
-    bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -X POST http://127.0.0.1:8080/job/PluralsightHelloWorlds/build?token=authenticated'
+    bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
 }

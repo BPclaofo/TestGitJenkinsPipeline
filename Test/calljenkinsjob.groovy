ab663c68a3d36996a8f66c329e35680ba3dffa21
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 17:42:30 +0000

    Minor change.

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,4 +1,4 @@
 node ('master') {
     def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -sX -o /dev/null -w "%%{http_code}" POST http://127.0.0.1:8080/job/PluralsightHelloWorlds/build?token=authenticated'
-    if (response != 000000201) {exit 1}
+    bat "IF NOT ${response} exit 1"
 }

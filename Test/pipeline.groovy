4e6617aa8705ddfec1e3a1d63c39fdcb2d5e2533
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 11:31:57 +0000

    Added a trigger for manual loading of a side groovy pipeline script.

--- Test/pipeline.groovy
+++ Test/pipeline.groovy
@@ -4,7 +4,7 @@ node('master') {
     git url: 'https://github.com/BPclaofo/parallel-test-executor-plugin-sample.git'
     stash includes: 'pom.xml, src/', name: 'GitFiles'
 }
-def splits = splitTests([$class: 'CountDrivenParallelism', size: 4])
+def splits = splitTests([$class: 'CountDrivenParallelism', size: 2])
 def branches = [:]
 for (int i = 0; i < splits.size(); i++) {
     def exclusions = splits.get(i);

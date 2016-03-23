e040c6c7193dcf1cd81b13cbe4ea1725e08dc9fe
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 15:18:23 +0000

    Adding a curl to start another Jenkins job.

--- Test/pipeline.groovy
+++ Test/pipeline.groovy
@@ -32,6 +32,10 @@ node {
     load 'Main/HelloWorld.groovy'
 }()
 
+node ('master') {
+    bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -X POST "http://localhost:8080/job/PluralsightHelloWorld/build?token=authenticated"'
+}
+
 @NonCPS
 def version(text) {
     def matcher = text =~ '<version>(.+)</version>'

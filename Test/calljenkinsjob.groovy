38171ecb546a5f8306ddce7ef3da1bd99899b8f5
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 17:00:08 +0000

    Modified curl command to only return repsonse code.

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,3 +1,4 @@
 node ('master') {
-    def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -sIX -w "%{http_code}" POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
+    //def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -sIX -w "%{http_code}" POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
+    def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -sX -o /dev/null -w "%{response_code}" POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
 }

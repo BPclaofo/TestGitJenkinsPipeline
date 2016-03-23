9cf395c800d2a259d79f0cd8e6c60faf056cc9fd
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 16:49:01 +0000

    Concatenating curl options.

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,3 +1,3 @@
 node ('master') {
-    def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -I -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
+    def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -sIX POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
 }

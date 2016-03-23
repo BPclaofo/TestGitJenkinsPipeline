7cd6a23ad9800301a608cda8ae7b8c27e0169b82
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 15:22:58 +0000

    Added new groovy pipeline job.

---
+++ Test/calljenkinsjob.groovy
@@ -0,0 +1,3 @@
+node ('master') {
+    bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -X POST http://localhost:8080/job/PluralsightHelloWorld/build?token=authenticated'
+}
--- Test/pipeline.groovy
+++ Test/pipeline.groovy
@@ -32,10 +32,6 @@ node {
     load 'Main/HelloWorld.groovy'
 }()
 
-node ('master') {
-    bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -X POST http://localhost:8080/job/PluralsightHelloWorld/build?token=authenticated'
-}
-
 @NonCPS
 def version(text) {
     def matcher = text =~ '<version>(.+)</version>'

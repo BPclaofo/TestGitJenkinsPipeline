abb6bc478c742c196d7a8660e9fcf36a16bb5c86
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 10:31:55 +0000

    Adding one of the initial pipeline scripts generated from the available tutorial on Git.

---
+++ Test/pipelinesimplt.groovy
@@ -0,0 +1,19 @@
+node('master') {
+    git url: 'https://github.com/BPclaofo/parallel-test-executor-plugin-sample.git'
+    stash includes: 'pom.xml, src/', name: 'GitFiles'
+}
+def splits = splitTests([$class: 'CountDrivenParallelism', size: 2])
+def branches = [:]
+for (int i = 0; i < splits.size(); i++) {
+    def exclusions = splits.get(i);
+    branches["split${i}"] = {
+        node('slave') {
+            deleteDir()
+            unstash 'GitFiles'
+            writeFile file: 'exclusions.txt', text: exclusions.join("\n")
+            bat "${tool 'DefaultMaven'}/bin/mvn -B -Dmaven.test.failure.ignore test"
+            step([$class: 'JUnitResultArchiver', testResults: 'target/surefire-reports/*.xml'])
+        }
+    }
+}
+parallel branches

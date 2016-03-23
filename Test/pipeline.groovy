553610b077879b2206e73226476b0e35ecf88bd2
Alan Clark <Alan.Clark3@bp.com>
2016-03-23 10:55:13 +0000

    Adding one of the initial pipeline scripts generated from the available tutorial on Git.

--- Test/pipeline.groovy
+++ Test/pipeline.groovy
@@ -1,37 +1,35 @@
 echo 'hello from Pipeline'
 echo TestParam
-node('master') {
-    input 'Run on master?'
-    git url: 'https://github.com/BPclaofo/simple-maven-project-with-tests.git'
-    def v = version(readFile('pom.xml'))
-    if (v) {
-        echo "Building version ${v}"
-    }
-    withEnv(["PATH+MAVEN=${tool 'DefaultMaven'}/bin"]){
-        bat 'mvn -B verify -Dmaven.test.failure.ignore verify'
-    }
-    // def mvnHome = tool 'DefaultMaven'
-    // env.PATH = "${mvnHome}/bin:${env.PATH}"
-    // bat 'mvn -B verify'
-    step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
-    step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+node('slave') {
+    input 'Run job?'
+    git url: 'https://github.com/BPclaofo/parallel-test-executor-plugin-sample.git'
+    archive 'pom.xml, src/'
 }
+def splits = splitTests([$class: 'CountDrivenParallelism', size: 4])
+def branches = [:]
+for (int i = 0; i < splits.size(); i++) {
+    def exclusions = splits.get(i);
+    branches["split${i}"] = {
+        node('slave') {
+            // deleteDir()
+            unarchive mapping: ['pom.xml' : '.', 'src/' : '.']
+            writeFile file: 'exclusions.txt', text: exclusions.join("\n")
+            def v = version(readFile('pom.xml'))
+            if (v) {
+                echo "Building version ${v}"
+            }
+            withEnv(["PATH+MAVEN=${tool 'DefaultMaven'}/bin"]){
+                bat 'mvn -B verify -Dmaven.test.failure.ignore verify'
+            }
+            step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
+            step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+        }
+    }
+}
+parallel branches
 
 @NonCPS
 def version(text) {
     def matcher = text =~ '<version>(.+)</version>'
     matcher ? matcher[0][1] : null
-}
-
-node('slave') {
-    input 'Run on slave?'
-    git url: 'https://github.com/BPclaofo/simple-maven-project-with-tests.git'
-    withEnv(["PATH+MAVEN=${tool 'DefaultMaven'}/bin"]){
-        bat 'mvn -B verify -Dmaven.test.failure.ignore verify'
-    }
-    // def mvnHome = tool 'DefaultMaven'
-    // env.PATH = "${mvnHome}/bin:${env.PATH}"
-    // bat 'mvn -B verify'
-    step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
-    step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 }

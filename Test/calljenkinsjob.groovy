36fb7be2528ab901328fb5d2e7484cf72aeab456
Alan Clark <Alan.Clark3@bp.com>
2016-03-29 13:44:21 +0100

    Minor modifications.

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,6 +1,6 @@
 node ('master') {
     stage 'Rest API Call'
-    def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated>tmp.txt & set /p Response=<tmp.txt & IF NOT %%Response%%==201 exit 1'
+    def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated%>tmp.txt & set /p Response=%<tmp.txt & IF NOT %%Response%%==201 exit 1'
     stage 'Condition Check'
     bat "IF NOT ${response}==0 echo Remote Jenkins job failed to start. Exit code (placeholder) & exit 1"
 }

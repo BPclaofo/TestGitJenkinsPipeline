c21e378cd1a0eadb2eb42ddbf9b3298ae2079ab3
Alan Clark <Alan.Clark3@bp.com>
2016-03-29 15:07:49 +0100

    Change to more batch script format.

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,6 +1,6 @@
 node ('master') {
     stage 'Rest API Call'
-    bat '''HTTPResp=C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated
+    bat '''HTTPResp=C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated
 IF HTTPResp==201 calc.exe'''
     //def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated^>tmp.txt ^& set /p Response=^<tmp.txt ^& IF NOT %%Response%%==201 exit 1'
     stage 'Condition Check'

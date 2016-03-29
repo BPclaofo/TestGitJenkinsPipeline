5416c2dfc097fcdfa91dba635348a3d570347e46
Alan Clark <Alan.Clark3@bp.com>
2016-03-29 15:20:08 +0100

    Minor changes.

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,7 +1,7 @@
 node ('master') {
     stage 'Rest API Call'
     response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
-    echo ${response}
+    echo "${response}"
     //response = bat '''HTTPResp=C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated
 //IF HTTPResp==201 calc.exe'''
     //def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated^>tmp.txt ^& set /p Response=^<tmp.txt ^& IF NOT %%Response%%==201 exit 1'

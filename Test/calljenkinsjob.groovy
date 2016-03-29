ec4af0a37104263c7044131fe4bd860c9eb94b0c
Alan Clark <Alan.Clark3@bp.com>
2016-03-29 11:59:55 +0100

    Proving that Jenkins call job will work if condition is set to response=0 (bash script run successfully).

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,4 +1,4 @@
 node ('master') {
     def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -sX -o /dev/null -w "%%{http_code}" POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
-    bat "IF NOT ${response}==000000201 echo Remote Jenkins job failed to start. Exit code (placeholder) & exit 1"
+    bat "IF NOT ${response}==0 echo Remote Jenkins job failed to start. Exit code (placeholder) & exit 1"
 }

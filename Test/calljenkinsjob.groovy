14b23d5cad12cb84f80a2fe9590e6955598d22d6
Alan Clark <Alan.Clark3@bp.com>
2016-03-29 13:05:13 +0100

    Modifications to curl arguments.

--- Test/calljenkinsjob.groovy
+++ Test/calljenkinsjob.groovy
@@ -1,6 +1,6 @@
 node ('master') {
     stage 'Rest API Call'
-    def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
+    def response = bat 'C:\\Users\\claofo\\Documents\\Curl\\curl.exe -s -w %%{http_code} -X POST http://127.0.0.1:8080/job/PluralsightHelloWorld/build?token=authenticated'
     stage 'Condition Check'
     bat "IF NOT ${response}==0 echo Remote Jenkins job failed to start. Exit code (placeholder) & exit 1"
 }

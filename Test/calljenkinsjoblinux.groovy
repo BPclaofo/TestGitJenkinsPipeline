ed947b833c895fa44985e3d7e45acd632a212179
Alan Clark <Alan.Clark3@bp.com>
2016-03-29 18:00:12 +0100

    Minor changes.

--- Test/calljenkinsjoblinux.groovy
+++ Test/calljenkinsjoblinux.groovy
@@ -1,5 +1,5 @@
 node ('master') {
     stage 'Rest API Call'
-    response = bash 'if [ `curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=test` = "201" ]; then echo "Success"; fi'
+    response = sh 'if [ `curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=test` = "201" ]; then echo "Success"; fi'
     echo "${response}"
 }

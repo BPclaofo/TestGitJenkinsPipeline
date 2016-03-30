d59b20894fd16aeb37bf28db200ffa69491315bd
Alan Clark <Alan.Clark3@bp.com>
2016-03-30 09:45:47 +0100

    Minor changes.

--- Test/calljenkinsjoblinux.groovy
+++ Test/calljenkinsjoblinux.groovy
@@ -1,6 +1,6 @@
 node ('master') {
     stage 'Rest API Call'
-    response = sh 'if [ `curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=test` != \"201\" ]; then return 13; fi'
+    response = sh 'HTTPrep=`curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=test`; if [HTTPrep = \"201\" ]; then exit 0; else exit HTTPrep; fi'
     stage 'Echo Rest API HTTP response'
     echo "${response}"
 }

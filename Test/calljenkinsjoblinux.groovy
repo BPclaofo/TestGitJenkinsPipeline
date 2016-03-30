d76c1d602c7f6757aab15c69d0eb7c5096e27a3b
Alan Clark <Alan.Clark3@bp.com>
2016-03-30 10:10:33 +0100

    Intentional incorrect Jenkins url.

--- Test/calljenkinsjoblinux.groovy
+++ Test/calljenkinsjoblinux.groovy
@@ -1,6 +1,6 @@
 node ('master') {
     stage 'Rest API Call'
-    response = sh 'curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=test'
+    response = sh 'curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkin/job/FirstTest/buildWithParameters?Parameter=test'
     stage 'Echo Rest API HTTP response'
     echo "${response}"
 }

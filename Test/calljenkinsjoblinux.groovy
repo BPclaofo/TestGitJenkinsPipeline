e497b986af4852dde19855877e2acad5d16f0287
Alan Clark <Alan.Clark3@bp.com>
2016-03-30 10:09:43 +0100

    Simplification test.

--- Test/calljenkinsjoblinux.groovy
+++ Test/calljenkinsjoblinux.groovy
@@ -1,6 +1,6 @@
 node ('master') {
     stage 'Rest API Call'
-    response = sh 'HTTPrep=`curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=test`; if [ $HTTPrep = 201 ]; then exit 0; else exit $HTTPrep; fi'
+    response = sh 'curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=test'
     stage 'Echo Rest API HTTP response'
     echo "${response}"
 }

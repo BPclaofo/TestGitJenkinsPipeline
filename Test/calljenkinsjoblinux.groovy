543624d0efedbe6242abd4fca6d8039c1a51a4d4
Alan Clark <Alan.Clark3@bp.com>
2016-03-30 13:40:14 +0100

    Most recent version of code.

--- Test/calljenkinsjoblinux.groovy
+++ Test/calljenkinsjoblinux.groovy
@@ -1,6 +1,27 @@
+stage 'Arbitrary Echo'
+echo "Hello from pipeline"
+
+stage 'Parameter Echo'
 node ('master') {
-    stage 'Rest API Call'
-    response = sh 'HTTPrep=`curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=test`; if [ $HTTPrep = 201 ]; then exit 0; else exit $HTTPrep; fi'
-    stage 'Echo Rest API HTTP response'
-    echo "${response}"
+    sh "echo ${Parameter1}"
+    sh "echo ${Parameter2}"
+    sh "echo ${Parameter3}"
+}
+
+stage 'Run Initial Job'
+node ('master') {
+    sh "HTTPrep=`curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/FirstTest/buildWithParameters?Parameter=${Parameter1}`; if [ \$HTTPrep = 201 ]; then exit 0; else exit \$HTTPrep; fi"
+    echo "Sleeping for 1 seconds"
+    sleep 1
+}
+
+stage 'Run Jobs in Parallel'
+node ('master') {
+    try {
+        sh "HTTPrep=`curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkin/job/FirstTest/buildWithParameters?Parameter=${Parameter2}`; if [ \$HTTPrep = 201 ]; then exit 0; else exit \$HTTPrep; fi"
+    } catch (e) {
+        def w = new StringWriter()
+        e.printStackTrace(new PrintWriter(w))
+    }
+    sh "HTTPrep=`curl -s -w %{http_code} -X POST http://reuxgbls535.bp.com:8110/jenkins/job/SecondTest/buildWithParameters?Parameter=${Parameter3}`; if [ \$HTTPrep = 201 ]; then exit 0; else exit \$HTTPrep; fi"
 }
